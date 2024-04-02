a47fc8c37bcb09ca8de8884b6410f1a46c2f391e
Luis Robles <luisr@mill5.com>
2024-04-01 21:07:09 -0500

    Creating the first Subscription test

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use the_newsletter::run;
+use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    run()?.await
+    let listener = TcpListener::bind("127.0.0.1:8000").expect("Couldn't bind port");
+    run(listener)?.await
 }
