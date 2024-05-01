e95ee2d31a9cf05d0133dabcb01aca80549821dd
Luis Robles <dracvs@gmail.com>
2024-04-30 19:05:29 -0500

    FMT and Clippy corrections with main branch.

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -49,7 +49,7 @@ until psql -h "$DB_HOST" -U "${DB_USER}" -p "${DB_PORT}" -d "postgres" - c '\q';
 done
 >&2 echo "Postgres is up and running on port ${DB_PORT}!"
 
-DATABASE_URL=posgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}
+DATABASE_URL=postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}
 export DATABASE_URL
 sqlx database create
 sqlx migrate run
